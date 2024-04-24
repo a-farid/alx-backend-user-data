9263f5b30d205e91e2715381b6f30db215f266d5
faridlord10 <fahizoune10@gmail.com>
2024-04-24 14:46:07 -0700

    db init trycatch

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -32,10 +32,10 @@ class DB:
     def add_user(self, email: str, hashed_password: str) -> User:
         """Add new user"""
         try:
-            new_user = User(email=hashed_password, hashed_password=hashed_password)
+            usr = User(email=hashed_password, hashed_password=hashed_password)
             self._session.add(User)
             self._session.commit()
         except Exception:
             self._session.rollback()
-            new_user=None
-        return new_user
+            usr = None
+        return usr
